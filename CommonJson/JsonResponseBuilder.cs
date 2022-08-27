b1af7cd2e579101313bbc15eea4c31aa38875ff7
Gradiyanto Putera Husein <gradiyanto@vsi.co.id>
2022-08-27 18:44:47 +0700

    common class to build json response

---
+++ CommonJson/JsonResponseBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CommonJson
+{
+    public class JsonResponseBuilder
+    {
+        private object _object;
+
+        public JsonResponseBuilder(object @object)
+        {
+            _object = @object;
+        }
+
+        public T Build<T>()
+        {
+            var option = GetDefaultOption();
+            string json = JsonSerializer.Serialize(_object, option);
+            return JsonSerializer.Deserialize<T>(json); ;
+        }
+
+        private JsonSerializerOptions GetDefaultOption()
+        {
+            var option = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.IgnoreCycles,
+                WriteIndented = true
+            };
+            return option;
+        }
+    }
+}
