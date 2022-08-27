46b63be073be1e2a0a1595bb4ac6ea6a725eae5a
Gradiyanto Putera Husein <gradiyanto@vsi.co.id>
2022-08-27 16:21:17 +0700

    service register extension

---
+++ MessageBus/ServiceRegister.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MessageBus
+{
+    public static class ServiceRegister
+    {
+        public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            var dockerConnectionString = builder.Configuration.GetConnectionString("Docker");
+            builder.Services.AddDbContext<DataContext>(options =>
+            options.UseMySql(dockerConnectionString, ServerVersion.AutoDetect(dockerConnectionString))
+            );
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            return builder;
+        }
+    }
+}
