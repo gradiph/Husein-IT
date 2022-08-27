974229f5e34c5c46f65d4f250ca51dd60f1be981
Gradiyanto Putera Husein <gradiyanto@vsi.co.id>
2022-08-27 16:21:26 +0700

    middleware extensions

---
+++ MessageBus/MiddlewareSetup.cs
@@ -0,0 +1,19 @@
+﻿namespace MessageBus
+{
+    public static class MiddlewareSetup
+    {
+        public static WebApplication RegisterMiddleware(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            return app;
+        }
+    }
+}
