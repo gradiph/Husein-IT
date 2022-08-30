1f1265896f56fa3f3282505fa33755933e25ed8c
Gradiyanto Putera Husein <gradiyanto@vsi.co.id>
2022-08-30 22:08:39 +0700

    add json formatter to serialize object/deserialize string

---
+++ CommonJson/JsonFormatter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace CommonJson
+{
+    public class JsonFormatter
+    {
+        public static string toString(object @object)
+        {
+            return JsonSerializer.Serialize(@object);
+        }
+
+        public static T toObject<T>(string @string)
+        {
+            return JsonSerializer.Deserialize<T>(@string);
+        }
+    }
+}
