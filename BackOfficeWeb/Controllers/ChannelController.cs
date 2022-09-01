a4e1eaccce4f57c9c1ee289802492f9c8154d669
Gradiyanto Putera Husein <gradiyanto@vsi.co.id>
2022-09-01 10:46:27 +0700

    add template view and controller for Channel

---
+++ BackOfficeWeb/Controllers/ChannelController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackOfficeWeb.Controllers
+{
+    public class ChannelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
